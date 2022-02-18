629e857f3e460b50130dc8e5ad1c163ce8af1da4
irhallac <ibrahimrizahallac@live.com>
2022-02-18 13:34:28 +0300

    Add files via upload

---
+++ ceng/com108/gcc_compiling/intro.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//comment 1
+
+/* comment2 
+
+comment3
+
+
+comment4
+
+*/
+int main(){
+
+    printf("hello ALKÜ \n");
+
+    printf("hello ALKÜ  CENG \n");
+ 
+    return 0;
+}
